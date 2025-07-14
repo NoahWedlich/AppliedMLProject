54ab41e93ad3f26b53adfd6e11973ff20d5eb59c
wedlichn <43807121+NoahWedlich@users.noreply.github.com>
2025-07-14 18:02:24 +0200

    moved svm validator

--- models/TunableKernelSVM,.py
+++ models/TunableKernelSVM,.py
@@ -5,7 +5,7 @@ from TunableModel import TunableModel
 
 class TunableKernelSVM(TunableModel):
     
-    def __init__(self, hyperparameters, validator=None):
+    def __init__(self, hyperparameters):
         kernel = hyperparameters.get('kernel', None)
         if kernel is None:
             raise ValueError("Kernel must be specified in hyperparameters.")
@@ -18,8 +18,16 @@ class TunableKernelSVM(TunableModel):
         super().__init__(
             model_class=svm.BinaryKernelSVM,
             hyperparameters=hyperparameters,
-            validator=validator
+            validator=self.validator
         )
+        
+    def validator(params):
+        if params['kernel'] == 'rbf':
+            return params['sigma'] > 0 and params['degree'] == 0 and params['intercept'] == 0
+        elif params['kernel'] == 'polynomial':
+            return params['sigma'] == 0 and params['degree'] > 0 and params['intercept'] >= 0
+        else:
+            return False
         
     def fit(self, X, y):
         return super().fit(X, y)
